84cb015808a7db49e47dc53f21a3afe2291bcc64
Udith Chandrarathna <udithsc@gmail.com>
2023-11-01 18:13:27 +0800

    added basic files

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Button } from './Button';
+export { default as Nav } from './Nav';
+export { default as PopularProductCard } from './PopularProductCard';
+export { default as ReviewCard } from './ReviewCard';
+export { default as ServiceCard } from './ServiceCard';
+export { default as ShoeCard } from './ShoeCard';
---
+++ src/sections/index.ts
@@ -0,0 +1,7 @@
+export { default as Hero } from './Hero';
+export { default as PopularProducts } from './PopularProducts';
+export { default as SuperQuality } from './SuperQuality';
+export { default as Services } from './Services';
+export { default as SpecialOffer } from './SpecialOffer';
+export { default as Subscribe } from './Subscribe';
+export { default as Footer } from './Footer';
