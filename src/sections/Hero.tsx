817fffe00d3e0ec41ab3f11030f478b9ce19a31c
Udith Chandrarathna <udithsc@gmail.com>
2023-11-02 13:03:34 +0800

    ts configs changes

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section
+      id='home'
+      className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
+    >
+      <div></div>
+    </section>
+  );
 };
 
 export default Hero;
