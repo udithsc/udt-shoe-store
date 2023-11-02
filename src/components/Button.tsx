6b3407a39a114d81406460b67f85c6b24aa4881b
Udith Chandrarathna <udithsc@gmail.com>
2023-11-02 18:24:13 +0800

    hero section updates

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+interface ButtonProps {
+  label: string;
+  iconUrl: string;
+  backgroundColor?: string;
+  textColor?: string;
+  borderColor?: string;
+  fullWidth?: boolean;
+}
 
-const Button = () => {
-  return <div>Button</div>;
-};
-
+const Button = ({
+  label,
+  iconUrl,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}: ButtonProps) => (
+  <button
+    className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+  ${
+    backgroundColor
+      ? `${backgroundColor} ${textColor} ${borderColor}`
+      : 'bg-coral-red text-white border-coral-red'
+  } rounded-full ${fullWidth && 'w-full'}
+  `}
+  >
+    {label}{' '}
+    {iconUrl && (
+      <img
+        src={iconUrl}
+        alt='button icon'
+        className='ml-2 rounded-full w-5 h-5'
+      />
+    )}
+  </button>
+);
 export default Button;
--- src/components/ShoeCard.tsx
+++ src/components/ShoeCard.tsx
@@ -1,7 +1,41 @@
-import React from 'react';
+interface ShoeCardProps {
+  imgUrl: {
+    thumbnail: string;
+    bigShoe: string;
+  };
+  bigShowImg: string;
+  changeBigShoeImage: (url: string) => void;
+}
 
-const ShoeCard = () => {
-  return <div>ShoeCard</div>;
+const ShoeCard = ({
+  imgUrl,
+  bigShowImg,
+  changeBigShoeImage,
+}: ShoeCardProps) => {
+  const handleClick = () => {
+    if (bigShowImg !== imgUrl.bigShoe) changeBigShoeImage(imgUrl.bigShoe);
+  };
+
+  return (
+    <div
+      className={`border-2 rounded-xl ${
+        bigShowImg === imgUrl.bigShoe
+          ? 'border-coral-red'
+          : 'border-transparent'
+      } cursor-pointer max:sm:flex-1`}
+      onClick={handleClick}
+    >
+      <div className='flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4'>
+        <img
+          src={imgUrl.thumbnail}
+          alt='show collection'
+          width={127}
+          height={103.34}
+          className='object-contain'
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ShoeCard;
